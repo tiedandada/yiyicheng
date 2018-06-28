97570d033215fcc7cd4002cea492a876454f48d8
tiedandada <472960566@qq.com>
2018-06-28 09:41:05 +0800

    gege

--- application/back/controller/Goodstype.php
+++ application/back/controller/Goodstype.php
@@ -3,7 +3,7 @@
 namespace app\back\controller;
 
 use think\Controller;
-use think\Request;
+use think\facade\Request;
 use think\Db;
 class Goodstype extends Base
 {
@@ -17,7 +17,7 @@ class Goodstype extends Base
 
     }
     function toadd_goods_type(Request $request){
-        $shopname=$request->param('shopname');
+        $shopname=Request::param('shopname');
 //        print_r($shopname);
         $selshop=Db::table('category')->where('class_name',$shopname)->find();
         if(empty($selshop)){
@@ -33,19 +33,20 @@ class Goodstype extends Base
     }
     function del_goods_type(){
 
-         $arr = Db::table('goods')
-            ->alias('c')
-            ->join('category','c.class_id=category.class_id')
+         $arr = Db::table('category')
             ->select();
-
+            // dump($arr);die;
     	return $this->fetch('Del_goods_type',['admin_name'=>$this->admin_name,'arr'=>$arr,'ddurl'=>$this->ddurl]);
     }
     function updel_goods_type(){
-        $arr['goods_id']=$_GET;
-        $data=Db::table('goods')->where('goods_id', $arr['goods_id']['id'])->delete();
+        $class_id=Request::param('id');
+       
+        $data=Db::table('category')->where('class_id',$class_id)->delete();
         if(empty($data)){
-            return 1;die;
+            return 0;
+        }else{
+        	return 1;
         }
-       return $this->del_goods_type();
+       
     }
 }
